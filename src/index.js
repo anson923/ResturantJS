3f0eeb22cdcd1ef9b15b1305cc492f621519ace5
AnsonTheDeveloper <ahikamiz0907@gmail.com>
2023-03-08 19:15:29 -0800

    Updated header styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import loadHeader from './module/header';
 import loadHome from './module/home';
 import loadFooter from './module/footer';
+import './styles/general.css';
 
 //Initial Website
 const initialWebsite = (() => {
--- src/module/header.js
+++ src/module/header.js
@@ -1,14 +1,30 @@
 import '../styles/header.css';
+import Logo from '../images/resLogo.png';
 
 function loadHeader() {
   const content = document.querySelector('.content');
+  const header = document.createElement('header');
+  
+  const logoPlaceholder = document.createElement('div');
+  logoPlaceholder.className = 'logoPlaceholder';
+  const logo = new Image();
+  logo.className = 'logoImage';
+  logo.src = Logo;
 
+  logoPlaceholder.appendChild(logo);
+  header.appendChild(logoPlaceholder);
 
-  const text = document.createElement('div');
-  text.textContent = "Hello World! I am Header!";
-  text.className = 'HeaderText';
+  const navbar = document.createElement('nav');
+  navbar.innerHTML = `
+  <ul class="links">
+    <li class="tab active" data-tab-target="#home"><a href="#home">Home</a></li>
+    <li class="tab" data-tab-target="#menu"><a href="#menu">Menu</a></li>
+    <li class="tab" data-tab-target="#contact"><a href="#contact">Contact</a></li>
+  </ul>`;
 
-  content.appendChild(text);
+  header.appendChild(navbar);
+
+  content.appendChild(header);
 }
 
 export default loadHeader;

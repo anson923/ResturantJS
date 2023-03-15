86881e0cb8a18e33b3baada848659a75e8acf0a8
AnsonTheDeveloper <ahikamiz0907@gmail.com>
2023-03-15 00:45:17 -0700

    Added CleanWebpackPlugin and update index.html template directory

--- webpack.common.js
+++ webpack.common.js
@@ -1,15 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Production',
-      template: path.resolve(__dirname,'dist/index.html'),
-      inject: false,
+      template: path.resolve(__dirname,'src/index.html'),
+      inject: true,
     }),
   ],
   module: {

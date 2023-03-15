27598cbc87ce4554334286071522f920d5a1689e
AnsonTheDeveloper <ahikamiz0907@gmail.com>
2023-03-14 21:58:04 -0700

    Added new menu foods

--- src/module/menu.js
+++ src/module/menu.js
@@ -1,16 +1,26 @@
 import '../styles/menu.css';
 import SpicyBurger from '../images/spicyBurger.jpeg';
+import SpicySpaghetti from '../images/spicySpaghetti.jpg';
+import SpicySalmonRoll from '../images/spicySalmonRoll.jpg';
+import SpicyKatsuRamen from '../images/SpicyKatsuRamen.jpg';
+import SpicyRice from '../images/SpicyRice.jpeg';
+import SpicyChicken from '../images/SpicyChicken.jpg';
+import SpicyTurkeySandwich from '../images/SpicyTurkeySandwich.jpg';
+import SpicyChickenSoup from '../images/SpicyChickenSoup.jpg';
+import SpicySausagePizza from '../images/SpicySausagePizza.jpg';
+import SpicyKoreanPork from '../images/SpicyKoreanPork.jpg';
+
 const FoodLibrary = [
-  {name: 'SpicyBurger', id: 1, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 2, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 3, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 4, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 5, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 6, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 7, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 8, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 9, price: 12.99, src:SpicyBurger},
-  {name: 'SpicyBurger', id: 10, price: 12.99, src:SpicyBurger},
+  {name: 'Spicy Burger', id: 1, price: 12.99, src:SpicyBurger},
+  {name: 'Spicy Spaghetti', id: 2, price: 18.99, src: SpicySpaghetti},
+  {name: 'Spicy Salmon Roll', id: 3, price: 15.99, src: SpicySalmonRoll},
+  {name: 'Spicy Katsu Ramen', id: 4, price: 20.99, src: SpicyKatsuRamen},
+  {name: 'Spicy Rice', id: 5, price: 13.99, src: SpicyRice},
+  {name: 'Spicy Chicken', id: 6, price: 22.99, src: SpicyChicken},
+  {name: 'Spicy Turkey Sandwich', id: 7, price: 19.99, src: SpicyTurkeySandwich},
+  {name: 'Spicy Chicken Soup', id: 8, price: 9.99, src: SpicyChickenSoup},
+  {name: 'Spicy Sausage Pizza', id: 9, price: 21.99, src: SpicySausagePizza},
+  {name: 'Spicy Korean Pork.jpg', id: 10, price: 17.99, src: SpicyKoreanPork},
 ];
 
 const createFoodItem = (food) => {
